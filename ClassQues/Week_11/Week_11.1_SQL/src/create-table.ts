3ac7040a1889ffb4dc536924c80b52bfb2853104
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-05-29 23:52:20 +0530

    changes in doc

--- ClassQues/Week_11/Week_11.1_SQL/src/create-table.ts
+++ ClassQues/Week_11/Week_11.1_SQL/src/create-table.ts
@@ -2,7 +2,7 @@ import { getClient } from "./utils";
 
 async function createTable() {
     const createUserTableQuery = `
-        CREATE TABLE Users (
+        CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL
