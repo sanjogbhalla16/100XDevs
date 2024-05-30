22b10dbc34d18a9cb9a4d623143d6967394fbb8e
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-05-30 23:53:54 +0530

    changes in postgresql

--- ClassQues/Week_11/Week_11.1_SQL/src/utils.ts
+++ ClassQues/Week_11/Week_11.1_SQL/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgresql://");
+    const client = new Client("postgresql://example_from_Neon");
     await client.connect();
     return client;
 }
